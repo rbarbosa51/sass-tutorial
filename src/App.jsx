b50cc125c17ab7bfe4f269387d84648ca12929e3
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-04-05 23:41:03 -0500

    Tutorial Files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Component1 from './Component1';
+
+export default function App() {
+  return (
+   <>
+    <h1>Tutorial</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas accumsan lacus vel facilisis volutpat est velit egestas. A scelerisque purus semper eget duis at tellus at. Adipiscing commodo elit at imperdiet dui accumsan. Consequat id porta nibh venenatis cras. Posuere sollicitudin aliquam ultrices sagittis. Maecenas sed enim ut sem viverra aliquet eget sit. In dictum non consectetur a erat nam at. Lorem mollis aliquam ut porttitor leo. Ut sem nulla pharetra diam sit. Dignissim enim sit amet venenatis urna cursus. A pellentesque sit amet porttitor. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Tellus in hac habitasse platea dictumst. Justo nec ultrices dui sapien eget mi proin sed libero. Posuere ac ut consequat semper viverra nam libero. Tellus in metus vulputate eu scelerisque felis imperdiet. In mollis nunc sed id semper risus in hendrerit gravida.</p>
+    <Component1 />
+   </>
+  )
+}
+
---
+++ src/Component1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Component1() {
+    return (
+        <form>
+            <label htmlFor="textInput">Write Something:</label>
+            <input id="textInput" type="text"></input>
+            <button>Do something</button>
+        </form>
+    );
+}
